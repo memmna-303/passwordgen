93155c204e934c399eaa4e32b24d6616143d6b67
memmna-303 <mennasamir049@gmail.com>
2024-06-20 14:41:32 +0300

    Add files via upload

---
+++ js/mod.js
@@ -0,0 +1,14 @@
+ let a=100;
+ function say(n){
+    return "hello "+n+"";
+
+}
+ let my=[1,3,4,5];
+ let x=my.filter(function(e){
+    return e<5;
+ })
+ export{
+    a as num,
+    say,
+    x as mar
+ };
